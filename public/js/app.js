fa26d95c1a062f9fd6f57ece2717009806dae7d4
mattyy42 <mikim2761@gmail.com>
2021-05-12 22:58:50 +0300

    front end done

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\admin\Desktop\final project\Digital-Building-Management-System-Portal\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\admin\Desktop\final project\Digital-Building-Management-System-Portal\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Digital-Building-Management-System-Portal\Digital-Building-Management-System-Portal\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Digital-Building-Management-System-Portal\Digital-Building-Management-System-Portal\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
