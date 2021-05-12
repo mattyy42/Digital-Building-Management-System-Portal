6e0df77b2f9f786405e47d9742cb7e8de944f1cd
mattyy42 <mikim2761@gmail.com>
2021-05-13 00:30:24 +0300

    registration and log in done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,24 +10,29 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-    
-                        <div class="form-group row">
-                            <label for="first_name" class="col-md-4 col-form-label text-md-right">First Name</label>
-
-                            <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control" name="first_name" >
-                                 @error('first_name')
-                                        <span class="invalid-feedback" role="alert">
-                                             <strong>{{ $message }}</strong>
-                                        </span>
-                                 @enderror
-                            </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="fname">First Name:</label>
+                        <div class="col-sm-10">          
+                           <input type="text" class="form-control" id="first_name" placeholder="Enter Name" name="first_name">
+                           <span class="text-danger">{{ $errors->first('first_name') }}</span>
                         </div>
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
+                     </div>
+                     <div class="form-group">
+                        <label class="control-label col-sm-2" for="last_name">Last Name:</label>
+                        <div class="col-sm-10">          
+                           <input type="text" class="form-control" id="last_name" placeholder="Enter Name" name="last_name">
+                           <span class="text-danger">{{ $errors->first('last_name') }}</span>
+                        </div>
+                     </div>
+                     <div class="form-group">
+                        <label class="control-label col-sm-3" for="fname">Phone Number:</label>
+                        <div class="col-sm-10">          
+                           <input type="text" class="form-control" id="phone_number" placeholder="phone number" name="phone_number">
+                           <span class="text-danger">{{ $errors->first('phone_number') }}</span>
+                        </div>
+                     </div>
+                     <div class="form-group">
+                     <label for="email" class="control-label col-sm-3">{{ __('E-Mail Address') }}</label><div class="col-md-10">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -36,27 +41,12 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                     </div>
+                    
+                    <div class="form-group">
+                            <label for="password" class="control-label col-sm-3">{{ __('Password') }}</label>
 
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+                            <div class="col-sm-10">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -67,10 +57,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="form-group ">
+                            <label for="password-confirm" class="control-label col-sm-3">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
