5e12f8b4bdb164145740872ce24e1b7e493cd266
mattyy42 <mikim2761@gmail.com>
2021-06-05 23:33:58 +0300

    application api 3

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::get('/user', function (Request $request) {
 //use api/register
 Route::post('/register','api\AutheticationController@register');
 Route::post('/login','api\AutheticationController@login');
+//application 
 Route::post('/appliant/submitAppliction/{id}','api\ApplicantController@storeApplication');
 Route::get('/applicant/viewApplication/{id}','api\ApplicantController@viewApplication');
 Route::get('/applicant/delete/{id}','api\ApplicantController@deleteApplication');
